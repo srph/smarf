4bc24f3eed06ae420c31198db2e186f14878bc6c
Kier Borromeo <seraphipod@gmail.com>
2022-05-01 13:14:22 +0800

    api: Fix missing namespace:

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BoardsController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\HeroesController;
