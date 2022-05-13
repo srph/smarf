6e8516d95473ffa2c9a3938fb45728f88b1be037
srph <seraphipod@gmail.com>
2022-05-13 09:51:12 +0800

    ui: Fix useLatestValue hook

--- ui/src/hooks/useLatestValue.ts
+++ ui/src/hooks/useLatestValue.ts
@@ -3,9 +3,7 @@ import { MutableRefObject, useEffect, useRef } from 'react'
 function useLatestValue<T>(value: T): MutableRefObject<T> {
   const ref = useRef(value)
 
-  useEffect(() => {
-    ref.current = value
-  }, [value])
+  ref.current = value
 
   return ref
 }
